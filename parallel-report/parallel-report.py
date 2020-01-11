105f2be6600b42da0458f8882d059f166f9d7fa2
PrismaticPolygon <domwhite97@gmail.com>
2020-01-11 18:39:52 +0000

    Finished report graph functions

--- parallel-report/parallel-report.py
+++ parallel-report/parallel-report.py
@@ -11,7 +11,7 @@ def amdahl(N, s, p):
 
     return 1 / (s + (p / N))
 
-def gustafson(N, s):
+def gustafson(N, s, p):
 
     return s + (p * N)
 
@@ -27,6 +27,9 @@ def format_func(value, tick_number):
     else:
 
         return float_str
+
+# Need to figure out graphing the weak. Then I'm good for the day. Am I? It's not very late.
+# I'll start thinking about my game. Most likely, too my dissertation script has finished running.
 
 def strong():
 
@@ -49,7 +52,7 @@ def strong():
 
     plt.plot(df["threads"], amdahl(df["threads"], params[0], params[1]), label='Fitted function', color="red")
 
-    plt.xlabel(r'Number of threads $n$')
+    plt.xlabel(r'Number of threads $N$')
     plt.ylabel(r'Speedup')
 
     plt.title(r"Amdahl's law")
@@ -61,23 +64,23 @@ def strong():
 
     plt.show()
 
+global t_1, s_1
+
+def scaled_speedup(row):
+
+    return (t_1 / row["runtime"]) * (row["size"] / s_1)
+
 def weak():
 
-    df = pd.read_csv("dummy.csv")
+    global t_1, s_1
 
-    # Hm. I don't think that we can equate speedup in the same way.
-    # Instead, we should be plotting
-    # Instead, we calculate scaled speedup, and plot that. Right?
-    # That's all that we can plot.
+    df = pd.read_csv("dummy.csv")
+    df = df[df["type"] == "weak"]
 
     t_1 = df.iloc[0]["runtime"]    # Time taken on 1 core
+    s_1 = df.iloc[0]["size"]       # Input size of first problem
 
-    df["speedup"] = df["runtime"].map(lambda x: t_1 / x)
-    # We put in x, then y.
-    # x is the number of threads
-    # y is the speedup.
-
-    df = df[df["type"] == "weak"]
+    df["speedup"] = df.apply(scaled_speedup, axis=1)
 
     plt.figure(figsize=(6, 3))  # Default is (6, 4)
 
@@ -85,12 +88,12 @@ def weak():
 
     params, params_covariance = optimize.curve_fit(gustafson, df["threads"], df["speedup"])
 
-    print("s:", params[0]) # s = 0.1 for Gustafson's law
+    print("s:", params[0])
     print("p:", params[1])
 
     plt.plot(df["threads"], gustafson(df["threads"], params[0], params[1]), label='Fitted function', color="red")
 
-    plt.xlabel(r'Number of threads $n$')
+    plt.xlabel(r'Number of threads $N$')
     plt.ylabel(r'Scaled speedup')
 
     plt.title(r"Gustafson's law")
@@ -108,59 +111,3 @@ if __name__ == "__main__":
     strong()
 
     weak()
-
-# It's... flat. Why? What the fuck does that mean? It's not at all parallelised?
-
-
-# https://www.kth.se/blogs/pdc/2018/11/scalability-strong-and-weak-scaling/
-
-# Strong scaling: how the solution time varies with the number of processors
-# for a fixed total problem size.
-
-# Weak scaling: how the solution time varies with the number of processors
-# for a fixed problem size per processor.
-
-# The speedup in parallel computing can be straightforwardly defined as:
-# t_1 / t_N, where t_1 is the computational time running on one processor,
-# and t_N is the computational time running on N processors.
-# Ideally, software should have a linear speedup.
-
-# Amdahl's law: speedup is limited b the fraction of the serial part of
-# the software not amenable to parallelisation:
-
-# speedup = 1 / (s + p / N), where s is the proportion of execution time
-# spent on the serial part, p is the proportion spent on the parallalised part,
-# and N is the number of processors. This means that, for a fixed problem,
-# the upper limit of speedup is determined by the serial fraction of the code.
-# This is STRONG scaling.
-
-# Gustafson's law is based on the approximations that:
-# a) the parallel part p scales linearly with the amount of resources
-# b) the serial part does not increas with respect to the size of the problem.
-
-# To test strong scaling: test how the overall computational time of the job
-# scales with the number of processing elements.
-
-# To test weak scaling, increase both the job size and the number
-# of processing elements.
-
-# For the strong scaling, we'll want to run with identical parameters
-# on a different number of threads (1 - 24).
-
-# For the weak scaling, we'll want to increase both the thread count and
-# the size of the thing.
-
-# schedule(dynamic) provides better workload distribution at the cost of some extra overhead.
-
-# This gives the formula:
-# scaled speedup = s + p x N.
-# The scaled speedup increases linearly with respect to the number of processors
-
-# This is going to be difficult to do until I get my supercomputer access.
-# Yikes.
-
-# Study the scalability of your code. Compare it to be a weak scaling
-# and a strong scaling model.
-# Switch OFF file output for your experiments.
-# Assess the quality of my parallelisation using tool support (correctness
-# and analysis).

20a3c79a2f4a3ee0d42985adc7225db500f59dfa
WHITE <dominic.white@durham.ac.uk>
2020-01-11 21:29:14 +0000

    Added weighted positions to step2

--- solution-step2.c
+++ solution-step2.c
@@ -197,13 +197,9 @@ void updateBody() {
   maxV   = 0.0;	// The highest velocity
   minDx  = std::numeric_limits<double>::max();	// The minimum distance between particles
   
-  double** forces = new double*[NumberOfBodies];	// A 2D array of the forces on each molecule [[fx1, fy1, fz1], [fx2, fy2, fz2], [fx3, fy3, fz3]]. Here because we can't modify other methods.
-  
-  for (int i = 0; i < NumberOfBodies; i++) {
-	  
-	  forces[i] = new double[3]{0.0, 0.0, 0.0};
-	  
-  }
+  double* forces0 = new double[NumberOfBodies]{0};
+  double* forces1 = new double[NumberOfBodies]{0};
+  double* forces2 = new double[NumberOfBodies]{0};
   
   // Iterate through the particles, from 0 to 1
   for (int i = 0; i < NumberOfBodies - 1; i++) {
@@ -221,23 +217,30 @@ void updateBody() {
 		  minDx = std::min( minDx,distance );
 		  
 		  if (distance < diameter) {
-			  
-			  for (int k = 0; k < 3; k++) {
-			  				
-				  v[i][k] = (mass[i] / (mass[i] + mass[j])) * v[i][k] + (mass[j] / (mass[i] + mass[j])) * v[j][k];
-					
-					
-			  }
+
+			 double newWeight = mass[i] + mass[j];
+			 double weight_i_over = mass[i] / newWeight;
+			 double weight_j_over = mass[j] / newWeight;
+
+			 v[i][0] = weight_i_over * v[i][0] + weight_j_over * v[j][0];
+			 v[i][1] = weight_i_over * v[i][1] + weight_j_over * v[j][1];
+			 v[i][2] = weight_i_over * v[i][2] + weight_j_over * v[j][2];
+
+			 x[i][0] += weight_j_over * (x[j][0] - x[i][0]);
+			 x[i][1] += weight_j_over * (x[j][1] - x[i][1]);
+			 x[i][2] += weight_j_over * (x[j][2] - x[i][2]);
+
+		     // This isn't right.
 			    			
-			  mass[i] += mass[j]; // Merge masses
+			 mass[i] = newWeight;
 			
-			for (int c = j; c < NumberOfBodies; c++) {	// Remove particle j by left-shifting subsequent particles 
+			 for (int c = j; c < NumberOfBodies; c++) {	// Remove particle j by left-shifting subsequent particles 
 			
 				x[c] = x[c+1];			// Co-ordinates
 				mass[c] = mass[c+1];	// Mass
 				v[c] = v[c+1];			//	Velocity
 			
-			}	
+			 }	
 			
 			 NumberOfBodies--;  
 			 j--;	// Decrement j as the "old" j has been deleted
@@ -245,14 +248,18 @@ void updateBody() {
 			  
 		  }	else {
 		
-			  for (int k = 0; k < 3; k++) {
-			  
-				  double force = (x[j][k] - x[i][k]) * mass[i] * mass[j] / distance / distance / distance ;
-				
-				  forces[i][k] += force;
-				  forces[j][k] -= force;
-				  
-			  }
+			  double force0 = (x[j][0] - x[i][0]) * mass[i] * mass[j] / distance / distance / distance;
+      		  double force1 = (x[j][1] - x[i][1]) * mass[i] * mass[j] / distance / distance / distance;
+      		  double force2 = (x[j][2] - x[i][2]) * mass[i] * mass[j] / distance / distance / distance;
+
+            forces0[i] += force0;
+      forces0[j] += -force0;
+
+      forces1[i] += force1;
+      forces1[j] += -force1;
+
+      forces2[i] += force2;
+      forces2[j] += -force2;
 		  
 		  }
 		  
@@ -262,19 +269,17 @@ void updateBody() {
   
   for (int i = 0; i < NumberOfBodies; i++) {	// Update positions and velocity of particles
 	  
-	  double totalV = 0;
+	  x[i][0] = x[i][0] + timeStepSize * v[i][0];
+	x[i][1] = x[i][1] + timeStepSize * v[i][1];
+	x[i][2] = x[i][2] + timeStepSize * v[i][2];  
+
+	v[i][0] = v[i][0] + timeStepSize * forces0[i] / mass[i];
+    v[i][1] = v[i][1] + timeStepSize * forces1[i] / mass[i];
+    v[i][2] = v[i][2] + timeStepSize * forces2[i] / mass[i];
+
+	double totalV = sqrt(v[i][0] * v[i][0] + v[i][1] * v[i][1] + v[i][2] * v[i][2]);
 	  
-	  for (int k = 0; k < 3; k++) {
-		  
-		  x[i][k] = x[i][k] + timeStepSize * v[i][k];	// Update particle i coordinates in dimension k
-		  
-		  v[i][k] = v[i][k] + timeStepSize * forces[i][k] / mass[i];	// Update particle i velocity in dimension k 
-		  
-		  totalV += v[i][k] * v[i][k];
-		  
-	  }
-	  
-	  maxV = std::max(maxV, std::sqrt(totalV));
+	  maxV = std::max(maxV, totalV);
 
   }
   
@@ -288,12 +293,9 @@ void updateBody() {
 
   t += timeStepSize;
   
-  for (int i = 0; i < NumberOfBodies; i+) { // Free up memory.
-
-        delete[] forces[i];
-  }
-
-  delete[] forces;
+  delete[] forces0;
+	delete[] forces1;	
+	delete[] forces2;
 
 }
 
